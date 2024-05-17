8d5512466cf5a1e9c6ec9ebce53d271e5bd7f8ff
WWeWenBininn <162079673+WWeWenBininn@users.noreply.github.com>
2024-05-17 13:32:58 +0800

    HMS.c
    main c file for HMS

--- HMS.c
+++ HMS.c
@@ -1,82 +1,64 @@
 #include <stdio.h>
 #include <string.h>
-
-
-
-struct credentials
-{
-    char username[30];
-    char password[30]; 
-};
-
-
-int login(struct credentials *credentials)
-{
-    char enteruser[50];
-    char enterpass[50];
-
-    printf("Enter username: ");
-    scanf("%s",&enteruser);
-    printf("Enter password: ");
-    scanf("%s",&enterpass);
-   
-        if (strcmp(enteruser, credentials[0].username) == 0 &&
-            strcmp(enterpass, credentials[0].password) == 0) {
-            printf("Login successful! \n");}
-    
-    
-
-
-}
+#include <stdbool.h>
+#include "login.c"
 
 
 int main()
 {
     int choice;
     printf("\n\n\n\n\n\nHospital Management System\n--------------------------\nWho's logging in?\n1. Admin\n2. Doctor\n3. Nurse\n4. Patient\n\n");
+    printf("Please enter department: ");
     scanf("%d",&choice);
-
     if(choice == 1)
-    {
-        struct credentials admins[] = 
+    { 
+        if(adminlogin())
         {
-            {"maryjane","janemary30"},
-            {"Jamespotter","harry"},
-            {"momochan","whatislove"}
-        
-        };
+            int e;
+            printf("press 1 if successful");
+            scanf("%d",&e);
+            if(e == 1)
+            {
+                printf("nice");
+            }
+            else{
+                printf("wtf");
+            }
+        }
+        else {
+            printf("Terminating...\n\n\n\n\n\n\n");
+        }
     }
-
-    else if(choice == 2)
+    else if (choice ==2)
     {
-        struct credentials doctors[] = 
+        if(doctorlogin())
         {
-            {"drparker","parking@4"},
-            {"drhouse","houseattheER"},
-            {"drwho","angels"}
-        
-        };
+            printf("yes2");
+        }
+        else{
+            printf("cant");
+        }
     }
-
-    else if(choice == 3)
+    else if (choice == 3)
     {
-        struct credentials nurse[] = 
+        if (nurselogin())
         {
-            {"nursejoy","pikachu"},
-            {"Kimnayeon","poppoppop"},
-            {"oliverjam","cooking"}
-        
-        };
+            printf("nice3");
+        }
+        else
+        {
+            printf("nope");
+        }
     }
-
-    else if(choice == 4)
+    else if (choice == 4)
     {
-        struct credentials patient[] = 
+        if (patientlogin())
         {
-            {"Jungkook","seven7"},
-            {"Harrystyles","goodnight"},
-            {"Macfarlane","Logic"}
-        
-        };
-    }
+            printf("nice4");
+        }
+        else
+        {
+            printf("eh");
+        }
+    }   
 }
