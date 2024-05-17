56e9530cf96cf29a03d09a67ab975adfe5021ac0
WWeWenBininn <162079673+WWeWenBininn@users.noreply.github.com>
2024-05-17 13:33:47 +0800

    creds.h
    credentials header for HMS

---
+++ creds.h
@@ -0,0 +1,28 @@
+typedef struct 
+{
+    char username[40];
+    char password[40];
+}creds;
+
+
+//user login details
+typedef char enteruser[40];
+typedef char enterpass[40];
+
+    enteruser user;
+    enterpass pass;
+    creds admin1 = {"maryjane","janemary30"};
+    creds admin2 = {"Jamespotter","harry"};
+    creds admin3 = {"momochan","whatislove"};
+
+    creds doc1 = {"drparker","parking@4"};
+    creds doc2 = {"drhouse","houseattheER"};
+    creds doc3 ={"drwho","angels"};
+
+    creds nur1 = {"nursejoy","pikachu"};
+    creds nur2 = {"Kimnayeon","poppoppop"};
+    creds nur3 = {"oliverjam","cooking"};
+
+    creds pat1 = {"Jungkook","seven7"};
+    creds pat2 = {"Harrystyles","goodnight"};
+    creds pat3 = {"Macfarlane","Logic"};
