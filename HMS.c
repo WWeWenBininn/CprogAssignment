fe69963afcf59093ef7228782db06c067df66411
WWeWenBininn <162079673+WWeWenBininn@users.noreply.github.com>
2024-05-20 20:34:48 +0800

    Update HMS.c

--- HMS.c
+++ HMS.c
@@ -10,8 +10,10 @@ int main()
     printf("\n\n\n\n\n\nHospital Management System\n--------------------------\nWho's logging in?\n1. Admin\n2. Doctor\n3. Nurse\n4. Patient\n\n");
     printf("Please enter department: ");
     scanf("%d",&choice);
-    if(choice == 1)
-    { 
+    switch (choice)
+    {
+        case 1:
+        
         if(adminlogin())
         {
             int e;
@@ -28,9 +30,10 @@ int main()
         else {
             printf("Terminating...\n\n\n\n\n\n\n");
         }
-    }
-    else if (choice ==2)
-    {
+        break;
+    
+        case 2:
+    
         if(doctorlogin())
         {
             printf("yes2");
@@ -38,27 +41,30 @@ int main()
         else{
             printf("cant");
         }
+        break;
+    
+        case 3:
+            if (nurselogin())
+            {
+                printf("nice3");
+            }
+            else
+            {
+                printf("nope");
+            }
+            break;
+        case 4:
+            if (patientlogin())
+            {
+                printf("nice4");
+            }
+            else
+            {
+                printf("eh");
+            }
+            break;
+        default:
+            printf("try again");
+            break;
     }
-    else if (choice == 3)
-    {
-        if (nurselogin())
-        {
-            printf("nice3");
-        }
-        else
-        {
-            printf("nope");
-        }
-    }
-    else if (choice == 4)
-    {
-        if (patientlogin())
-        {
-            printf("nice4");
-        }
-        else
-        {
-            printf("eh");
-        }
-    }   
 }
