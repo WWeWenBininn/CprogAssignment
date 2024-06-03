c8262d159f6b73a369c4c735f84b139da273092d
WWeWenBininn <162079673+WWeWenBininn@users.noreply.github.com>
2024-06-03 23:31:21 +0800

    Update creds.h
    (NOT FINAL) added chiaxin minyi and joshanns part

--- creds.h
+++ creds.h
@@ -1,6 +1,70 @@
 #define MAX_LINE_LENGTH 100
 #define MAX_SCHEDULES 100
+#define MAX_DOCTORS 10
+#define MAX_NAME_LENGTH 50
+#define HOURS_IN_DAY 24
+#define DAYS_IN_WEEK 7
+#define MAX_ITEMS 50
+#define RESTOCK_LEVEL 20
+#define LAST_STOCK 50
 
+//structs for admin section
+
+struct Inventory
+{
+    char medicinename[50];
+    int quantity;
+};
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
+struct newPatient
+{
+    char username[100];
+    char password[100];
+    char fullName[100];
+    int age;
+    char gender;
+    struct Date dob;
+    char ic[20];
+};
+
+struct newDoctor
+{
+    char username[100];
+    char password[100];
+    char fullName[100];
+    int age;
+    char gender;
+    struct Date dob;
+    char ic[20];
+};
+
+struct newNurse
+{
+    char username[100];
+    char password[100];
+    char fullName[100];
+    int age;
+    char gender;
+    struct Date dob;
+    char ic[20];
+};
+
+
+typedef struct {
+  char name[MAX_NAME_LENGTH];
+  char days[7][10]; // Array to store schedule for each day (exp., "Mon", "Tue")
+  char timings[7][20]; // Array to store timings for each day (exp., "9:00AM - 12:00PM")
+  char availability[7][20]; // Array to store doctor availability for each day (exp., "14:00PM - 17:00PM")
+} Doctor;
+
+
+//user login details
 typedef struct 
 {
     char ID[20];
@@ -8,10 +72,6 @@ typedef struct
     char username[40];
     char password[40];
 }creds;
-
-
-//user login details
-
 
     char user[40];
     char pass[40];
